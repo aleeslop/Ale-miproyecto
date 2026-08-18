439091d2eae504600eec24801e8fd643940d9492
alejandra-espinosa.md <aleeslop>
2026-08-18 11:38:42 -0600

    [M9-AL] Front con fetch - Alejandra: servidor actualizado

--- servidor/index.ts
+++ servidor/index.ts
@@ -1,9 +1,27 @@
-import type { Request, Response } from 'express';
-const express = require('express');
+import express, { Request, Response } from 'express';
+
 const app = express();
 
+// Middleware para permitir CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Lista fija de alumnos
+const alumnos = [
+  { nombre: 'Ruben', matricula: '22E31' },
+  { nombre: 'Luis', matricula: '22E32' },
+  { nombre: 'Mireya', matricula: '22E33' }
+];
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hola desde escuela');
+  res.send('Hola desde escuela con TypeScript');
+});
+
+app.get('/alumnos', (req: Request, res: Response) => {
+  res.json(alumnos);
 });
 
 app.listen(3000, () => {
