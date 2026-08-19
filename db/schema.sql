2cec71c0232e15c7e6e4fd64dca9efec85a75aaa
alejandra-espinosa.md <aleeslop>
2026-08-18 22:01:17 -0600

    Agregar scripts de base de datos de alumnos

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE alumnos (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100),
+  matricula VARCHAR(5),
+  grado VARCHAR(50)
+);
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO alumnos (nombre, matricula, grado)
+VALUES
+  ('Ruben', '22E31', 'Primero'),
+  ('Luis', '22E32', 'Segundo'),
+  ('Mireya', '22E33', 'Tercero');
